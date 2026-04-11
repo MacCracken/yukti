f016028d901bd16ff0cb71cb32322bdc565b64b5
Robert MacCracken <robert.maccracken@gmail.com>
2026-04-11 16:25:24 -0700

    bye felicia

--- rust-old/fuzz/fuzz_targets/fuzz_mount_table.rs
+++
@@ -1,9 +0,0 @@
-#![no_main]
-use libfuzzer_sys::fuzz_target;
-use std::path::Path;
-
-fuzz_target!(|data: &[u8]| {
-    if let Ok(table) = std::str::from_utf8(data) {
-        let _ = yukti::storage::find_mount_in(Path::new("/dev/sda1"), table);
-    }
-});
